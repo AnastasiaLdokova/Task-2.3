197c3fb617b54355504281bcf4bb51e522da7f98
Anastasia.L <easy.club.info@gmail.com>
2024-03-26 12:54:32 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        int initialamount = 100;
+        int addedAmount = 2000;
+        int bonus = addedAmount / 100;
+        boolean b = addedAmount >= 1000;
+        long totalSum = initialamount + addedAmount + bonus;
+        {
+            System.out.println("Уважаемый клиент! Спасибо, что Вы с нами всё это время!" +
+                    "За пополнение на счёт от 1000 рублей Вам начислятся приятные бонусы!" +
+                    "А куда их тратить - решать только Вам!");
+        }
+        {
+            System.out.println("Сумма на Вашем счёте: " + totalSum);
+        }
+
+        {
+            System.out.println("Сумма начисленных бонусных баллов: " + bonus);
+        }
+    }
+}
